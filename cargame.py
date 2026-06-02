8cec868fdba8a8dfc68aeefc5a5d527edefbf477
Drone1112 <jamiedunnet@cromwell.school.nz>
2026-06-03 09:56:36 +1200

    chore: cars need to move

--- cargame.py
+++ cargame.py
@@ -1,12 +1,11 @@
 ### this program is a llama game for playing
 
+#Imports
 import pygame
-import time
 import random
 
-pygame.init()
-
 # Screen Size and Screen Create
+pygame.init()
 screen_x = 750
 screen_y = 800
 screen = pygame.display.set_mode((screen_x, screen_y))
@@ -15,7 +14,6 @@ screen = pygame.display.set_mode((screen_x, screen_y))
 pygame.display.set_caption("Car Vroom game")
 game_icon = pygame.image.load("game_icon.png")
 pygame.display.set_icon(game_icon)
-
 
 #Colors
 gray = (140, 140, 140)
@@ -26,19 +24,29 @@ score = 0
 high_score = 0
 textx = 10
 texty = 10
+font = pygame.font.Font("freesansbold.ttf", 20)
+
+#Players car variables
 car_x = 300
 car_y = 650
 car_h = 95 
 car_w = 150
+car_x_change = 0
+car_y_change = 0
+
+#NPC car variables
 cars_y = 0
 cars_x = 0
 cars_w = 150 
 cars_h = 95
-car_x_change = 0
-car_y_change = 0
+cars_y_change = 0
+
+#Line and road variables
 line_size_h = 2000
 line_size_w = 10
-font = pygame.font.Font("freesansbold.ttf", 20)
+lane1_x = 225
+lane2_x = 375
+lane3_x = 525
 
 # Text create
 def message(msg, txt_colour, bkgd_colour):
@@ -55,6 +63,7 @@ def show_score(x, y):
     screen.blit(score_text, (x, y))
     screen.blit(hi_text, (x, y + 25))
 
+#Import high score
 def load_high_score():
     try:
         with open("highsocre.txt", "r") as hi_score_file:
@@ -68,19 +77,22 @@ def load_high_score():
         return 0
     return int(value)
 
+#Export highscore
 def save_high_score(value):
     with open("highsocre.txt", "w") as hi_score_file:
         hi_score_file.write(str(value))
 
 high_score = load_high_score()
 
+#NPC car creating
 class cars:
 
-    def __init__(self, cars_x, cars_y, cars_image, name):
+    def __init__(self, cars_x, cars_y, cars_image, name, speed):
         self.cars_x = cars_x
         self.cars_y = cars_y
         self.cars_image = cars_image
         self.name = name
+        self.speed = speed
 
     def make_cars(self):
         cars_rect = pygame.Rect(self.cars_x, self.cars_y, 0, 0)
@@ -90,13 +102,15 @@ class cars:
         pipe_flip = pygame.transform.flip(resized_cars, False, True)
         screen.blit(pipe_flip, cars_rect)
 
+    def move(self):
+        self.cars_y += self.speed
 
 #def cars_num(pixel):
     #cars_loc = round(random.randrange(20, pixel - 20))
     #return cars_loc
 
 
-
+#Gameloop
 while not quit_game:
     #quit logic
     for event in pygame.event.get():
@@ -115,12 +129,14 @@ while not quit_game:
 
     car_x += car_x_change
     
-    cars1 = cars(175,40, 2, "Green Car")
-    cars2 = cars(350, 40, 3, "Blue Car")
-    cars3 = cars(525,40, 4, "Orange Car")
-    cars4 = cars(700,40, 5, "Purple Car")
-    cars5 = cars(0,0, 6, "Sky Car")
-    cars_list = [cars1, cars2,cars3,cars4,cars5]
+    cars1 = cars(110,40, 2, "Green Car", 10)
+    cars2 = cars(260, 40, 3, "Blue Car", 10)
+    cars3 = cars(410,40, 4, "Orange Car", 10)
+    cars4 = cars(560,40, 5, "Purple Car", 10)
+    cars5 = cars(-100,0, 6, "Sky Car", 10)
+    cars_list = [cars1, cars2, cars3, cars4, cars5]
+
+    
 
     #Bondries
     if car_x >= (screen_x -90):
@@ -136,14 +152,22 @@ while not quit_game:
     show_score(textx, texty)
 
     for items in cars_list:
-            items.make_cars()
+        items.make_cars()
+        items.move(cars_list)
     
     #lines
-    ground_rect = pygame.Rect(400,800  - line_size_h, line_size_w, line_size_h)
+    ground_rect = pygame.Rect(lane1_x,800  - line_size_h, line_size_w, 
+                              line_size_h)
     pygame.draw.rect(screen, white, ground_rect)
 
+    ground_rect = pygame.Rect(lane2_x,800  - line_size_h, line_size_w, 
+                              line_size_h)
+    pygame.draw.rect(screen, white, ground_rect)
 
-            
+    ground_rect = pygame.Rect(lane3_x,800  - line_size_h, line_size_w, 
+                              line_size_h)
+    pygame.draw.rect(screen, white, ground_rect)
+
 
     #Players Car Makeing
     car = pygame.Rect(car_x, car_y, car_h, car_w)
